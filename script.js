d72b919b00decd112c8b08a03374162d3e535677
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-07-25 00:47:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function playagain() {
   document.querySelector('.message').textContent = "Start Guessing";
   document.querySelector('.number_box').style.visibility = 'visible';
   document.querySelector(".chancenumber").textContent = chancesvalue = 100;
-  document.querySelector('.assignscore').textContent = `${guessnumber}`; // Clear the previous guess number
+  document.querySelector('.assignscore').textContent = `${chancesvalue}`; // Clear the previous guess number
   document.getElementById('input').value = "";
 
   generateRandomNumber(); // Generate a new random number
