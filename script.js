637cfb9b14aacfe42f2cbadfdb47cf4501cd5f80
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-07-25 02:07:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ function playagain() {
   document.querySelector('.message').textContent = "Start Guessing";
   document.querySelector('.number_box').style.visibility = 'visible';
   document.querySelector(".chancenumber").textContent = chancesvalue = 100;
-  document.querySelector('.assignscore').textContent = `High Score: ${highscoring}`;
+  document.querySelector('.assignscore').textContent =`High Score: ${highscoring}`;
+
   }
   document.getElementById('input').value = "";
 
